305d500ecbca88b25c272784485a4fc87e1774eb
s3502942 <s3502942@student.rmit.edu.au>
2016-08-03 22:26:20 +1000

    added CDN's and css folder

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
+<!-- Lets all do our part in making our code clean :) --> 
+
 <!DOCTYPE html>
 
 <html>
 <head>
-    <title>WDA A1 ticketing bs</title>
+	<!-- css stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <title>Web Ticketing</title>
+
 </head>
+
 <body>
     
 </body>
+<footer>
+	<!-- jQuery CDN -->
+	<script src="jquery-1.12.4.min.js"></script>
+
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+
+	<!-- Compiled and minified JavaScript -->
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+</footer>
+
 </html>
+
+
