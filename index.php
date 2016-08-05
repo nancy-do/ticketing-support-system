8f75adda0732a78988f8b79d421794d318162b12
s3502942 <s3502942@student.rmit.edu.au>
2016-08-05 14:50:27 +1000

    Trying out nav bar designs

--- index.php
+++ index.php
@@ -8,20 +8,32 @@
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
     <title>Web Ticketing</title>
 
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
 </head>
 
 <body>
-    
+<nav>
+	<div class="nav-wrapper">
+		<a href="#!" class="brand-logo"><i class="material-icons"></i>ANIBI</a>
+		<ul class="right hide-on-med-and-down">
+			<li><a href="" class="material-icons">search</a></li>
+			<li><a href="" class="material-icons">view_module</a></li>
+			<li><a href="" class="material-icons">refresh</a></li>
+			<li><a href="" class="material-icons">more_vert</a></li>
+		</ul>
+	</div>
+</nav>
+
+<p>Gitit? Gitit?</p>
 </body>
 <footer>
 	<!-- jQuery CDN -->
 	<script src="jquery-1.12.4.min.js"></script>
 
-	<!-- Compiled and minified CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
-
-	<!-- Compiled and minified JavaScript -->
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
 </footer>
 
 </html>
