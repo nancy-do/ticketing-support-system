f72f0f9636f8696631a1425ebb351008a818d4f1
s3502942 <s3502942@student.rmit.edu.au>
2016-08-05 17:45:10 +1000

    Trying out nav bar designs

--- index.php
+++ index.php
@@ -6,35 +6,29 @@
 <head>
 	<!-- css stylesheet -->
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-    <title>Web Ticketing</title>
-
+    <title>ITS Web Ticketing System</title>
 	<!-- Compiled and minified CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
-
-	<!-- Compiled and minified JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+	<link rel="stylesheet" href="css/materialize.css">
 </head>
 
 <body>
-<nav>
-	<div class="nav-wrapper">
-		<a href="#!" class="brand-logo"><i class="material-icons"></i>ANIBI</a>
+<div class="materialize-red.lighten-2">
+	<nav>
+		<a href="index.php" class="brand-logo","material-icons">&nbsp ITS Web Ticketing System</a>
 		<ul class="right hide-on-med-and-down">
 			<li><a href="" class="material-icons">search</a></li>
-			<li><a href="" class="material-icons">view_module</a></li>
-			<li><a href="" class="material-icons">refresh</a></li>
-			<li><a href="" class="material-icons">more_vert</a></li>
 		</ul>
+	</nav>
 	</div>
-</nav>
-
-<p>Gitit? Gitit?</p>
 </body>
 <footer>
-	<!-- jQuery CDN -->
-	<script src="jquery-1.12.4.min.js"></script>
 
 </footer>
+<!-- jQuery CDN -->
+<script src="jquery-1.12.4.min.js"></script>
+
+<!-- Compiled and minified JavaScript -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
 
 </html>
 
