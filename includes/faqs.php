d9943b07bf1aa86c315b677ea0deda2aab34852f
Ian Jun Wei Tan <s3502942@csitprdap03.int.its.rmit.edu.au>
2016-08-17 12:41:48 +1000

    re cleaned dir

---
+++ includes/faqs.php
@@ -0,0 +1,24 @@
+	<div class="container w">
+		<div class="row centered">
+
+			<div class="col-lg-4">
+		    
+			</div><!-- col-lg-4 -->
+
+			<div class="col-lg-4">
+				<i class="fa fa-laptop"></i>
+					<h4>FREQUENTLY ASKED QUESTIONS</h4>
+					<p><strong>Q: </strong>How long will it take for my queries to be answered?</p>
+					<p><strong>A: </strong>Within 2 business days.</p>
+					<p><strong>Q: </strong>How do I view my submitted ticket?</p>
+					<p><strong>A: </strong>Click on view ticket in the home page to view your ticket.</p>
+					<p><strong>Q: </strong>How long will it take for my queries to be answered?</p>
+					<p><strong>A: </strong>Within 2 business days.</p>
+			</div><!-- col-lg-4 -->
+
+			<div class="col-lg-4">
+		    
+			</div><!-- col-lg-4 -->
+		</div><!-- row -->
+
+	</div><!-- container -->
---
+++ includes/navbar.php
@@ -0,0 +1,32 @@
+<body>
+
+<!-- Fixed navbar -->
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href='index.php?page=home'>IT SUPPORT <i class="fa fa-circle"></i> RMIT</a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href='index.php?page=home'>Home</a></li>
+        <li><a href='index.php?page=faqs'>Faqs</a></li>
+        <!-- <li><a data-toggle="modal" data-target="#myModal" href="#myModal"><i class="fa fa-envelope-o"></i></a></li> -->
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
+
+<div id="headerwrap">
+  <div class="container">
+    <div class="row centered">
+      <div class="col-lg-8 col-lg-offset-2">
+        <h1>ITS <br><b>Web Ticketing System</b></h1>
+      </div>
+    </div><!-- row -->
+  </div><!-- container -->
+</div><!-- headerwrap -->
