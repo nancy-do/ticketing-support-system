18f31542a8a87cc5bb10ce414a8901160ff4b121
blacksfk <s3333113@student.rmit.edu.au>
2016-08-17 13:29:43 +1000

    rere cleaned ticket

--- includes/Ticket.php
+++ includes/Ticket.php
@@ -27,12 +27,12 @@ class Ticket
         return $this->id;
     }
 
-    public function getLast()
+    public function getLastName()
     {
         return $this->lastName;
     }
 
-    public function getFirst()
+    public function getFirstName()
     {
         return $this->firstName;
     } 
@@ -83,29 +83,3 @@ class Ticket
         $this->comments[] = $comment;
     }
 }
-
-/* debug bs - uncomment to test */
-$ticket = new Ticket(session_id(), "Johhny", "Kronsky", "johnnyk@gmail.com", "Windows 7", "DOESN'T START MOFO", "I PRESS THE BUTTON AND IT DOESN'T WORK");
-$ticket->addComment("ITS: YOUR PC IS FUCKED BRAH");
-?>
-<!doctype html>
-<html>
-<body>
-    <div>
-        <?php
-            echo $ticket->getID() . "<br>";
-            echo $ticket->getFirst() . $ticket->getLast() . "<br>";
-            echo $ticket->getEmail() . "<br>";
-            echo $ticket->getOS() . "<br>";
-            echo $ticket->getIssue() . "<br>";
-
-            $array = $ticket->getComments();
-            
-            for ($i = 0; $i < count($array); $i++)
-            {
-                echo $array[$i] . "<br>";
-            }
-        ?>
-    </div>
-</body>
-</html>
