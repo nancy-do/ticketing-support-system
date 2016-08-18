19f8f4b2dfe381ceaca4fb95f8dd18c3e8f4cf0c
s3502942 <s3502942@users.noreply.github.com>
2016-08-19 01:02:43 +1000

    the one we did in the lab

--- js/validateForm.js
+++ js/validateForm.js
@@ -4,7 +4,7 @@
 $("#submit").click(function(){
 
     var rName = new RegExp(/^[a-z\s]{1,20}$/i);
-    var rEmail = new RegExp (/^([a-z0-9]+@(student\.)?(ems\.)?rmit\.edu\.au)$/i);
+    var rEmail = new RegExp (/[a-z0-9]+@([a-z.]{1,}).com|au/gi);
     var rIssue = new RegExp (/^.{1,}$/i);
 
     /* Check if First name or Last name is valid */
