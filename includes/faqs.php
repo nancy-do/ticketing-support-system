7d93a158ae3d98cbe82afe89fed6be59e689662a
WaiTungYuen <s3372851@student.rmit.edu.au>
2016-08-20 01:15:44 +1000

    - Some new features - Others fixes

--- includes/faqs.php
+++ includes/faqs.php
@@ -1,24 +1,49 @@
 	<div class="container w">
 		<div class="row centered">
 
-			<div class="col-lg-4">
-		    
-			</div><!-- col-lg-4 -->
+			<div class="col-lg-4" id="faq">
 
-			<div class="col-lg-4">
-				<i class="fa fa-laptop"></i>
-					<h4>FREQUENTLY ASKED QUESTIONS</h4>
-					<p><strong>Q: </strong>How long will it take for my queries to be answered?</p>
-					<p><strong>A: </strong>Within 2 business days.</p>
-					<p><strong>Q: </strong>How do I view my submitted ticket?</p>
-					<p><strong>A: </strong>Click on view ticket in the home page to view your ticket.</p>
-					<p><strong>Q: </strong>How long will it take for my queries to be answered?</p>
-					<p><strong>A: </strong>Within 2 business days.</p>
-			</div><!-- col-lg-4 -->
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8" id="main">
+							<h4>FREQUENTLY ASKED QUESTIONS</h4>
+							<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+								<div class="panel panel-default">
+									<div class="panel-heading" role="tab" id="headingOne">
+										<h4 class="panel-title">
+											<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+												<strong>Q: </strong>How long will it take for my queries to be answered?
+											</a>
+										</h4>
+									</div>
+									<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+										<div class="panel-body">
+											<ul>
+												<strong>A: </strong>Within 2 business days.
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="panel panel-default">
+									<div class="panel-heading" role="tab" id="headingTwo">
+										<h4 class="panel-title">
+											<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+												<strong>Q: </strong>How do I view my submitted ticket?
+											</a>
+										</h4>
+									</div>
+									<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+										<div class="panel-body">
+											<strong>A: </strong>Click on view ticket in the home page to view your ticket.
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
-			<div class="col-lg-4">
-		    
-			</div><!-- col-lg-4 -->
 		</div><!-- row -->
 
 	</div><!-- container -->
