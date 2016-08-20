08236ed40cfc9342bf813479122785482fe5568b
WaiTungYuen <s3372851@student.rmit.edu.au>
2016-08-20 10:05:19 +1000

    - Some new features - Others fixes

--- includes/faqs.php
+++ includes/faqs.php
@@ -19,7 +19,7 @@
 									<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
 										<div class="panel-body">
 											<ul>
-												<strong>A: </strong>Within 2 business days.
+												<strong>A: Within 2 business days.</strong>
 											</ul>
 										</div>
 									</div>
@@ -34,7 +34,87 @@
 									</div>
 									<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 										<div class="panel-body">
-											<strong>A: </strong>Click on view ticket in the home page to view your ticket.
+											<ul>
+												<strong>A: Click on view ticket in the home page to view your ticket.</strong>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="panel panel-default">
+									<div class="panel-heading" role="tab" id="headingThree">
+										<h4 class="panel-title">
+											<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+												<strong>Q: </strong>How to get started with Gmail?
+											</a>
+										</h4>
+									</div>
+									<div id="collapseThree" class="panel-collapse collapse" role="tabpane1" aria-labelledby="headingThree">
+										<div class="panel-body">
+											<ul>
+												<strong>A: Get started with Gmail</strong> <br/><br/>
+												<strong> Step 1: Access your Gmail account </strong> <br/><br/>
+												1. Go to www.gmail.com <br/>
+												2. Enter your RMIT email address (eg.s1234567@studen.rmit.edu.au) <br/>
+												3. Click <strong>Next</strong> <br/>
+												4. Enter your <strong>password</strong> <br/>
+												5. Click <strong>Sign in</strong> to open your Gmail account <br/><br/>
+												<strong> Step2: Compose an email</strong> <br/><br/>
+												1. In your Gmail <strong>Inbox</strong>, click <strong>COMPOSE</strong> <br/>
+												2. Add the recipents in the <strong>TO</strong> field <br/>
+												3. Add a subject <br/>
+												4. Add the body text. <br/><br/>
+												<strong>Step3: Format and Send an email</strong> <br/><br/>
+												1. In the tool bar, you can add Attachments, format font styles etc <br/>
+												2. Click <strong>Send.</strong> <br/><br/>
+												RMIT Staff. For more information, see: http://www1.rmit.edu.au/browse;ID=9wl5qtrmkjw9 <br/>
+												RMIT Students. For more information, see: http://www1.rmit.edu.au/browse;ID=wgjhl8p0h6jq1 <br/>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="panel panel-default">
+									<div class="panel-heading" role="tab" id="headingFour">
+										<h4 class="panel-title">
+											<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+												<strong>Q: </strong>How to connect to the RMIT-University wifi using Windows?
+											</a>
+										</h4>
+									</div>
+									<div id="collapseFour" class="panel-collapse collapse" role="tabpane1" aria-labelledby="headingFour">
+										<div class="panel-body">
+											<ul>
+												<strong>A: Connect to the RMIT-University wifi using Windows</strong> <br/><br/>
+												Please note: although newer versions of Windows will have a different look to the captures below, the steps should remain the same. <br/><br/>
+												1. Click on thre wireless icon in the system tray <br/>
+												2. Select RMIT-University from the list and click Connect <br/>
+												3.  Enter your student ID in Username and your RMIT password in Password. Click OK. <br/><br/>
+												If your Windows device is unable to make a connection via the steps above, you can attempt to set up the connection manually by following the guide below: <br/>
+												Manually Set Up The RMIT-University Network On Windows Devices <br/>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="panel panel-default">
+									<div class="panel-heading" role="tab" id="headingFive">
+										<h4 class="panel-title">
+											<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+												<strong>Q: </strong>How to login to myRMIT?
+											</a>
+										</h4>
+									</div>
+									<div id="collapseFive" class="panel-collapse collapse" role="tabpane1" aria-labelledby="headingFive">
+										<div class="panel-body">
+											<ul>
+												<strong>A: Accessing myRMIT</strong> <br/><br/>
+												<strong>1. Go to www.rmit.edu.au and click on the MyRMIT link at the top of the page</strong> <br/>
+												2. Log in with your student ID and password <br/>
+												3. Once logged in you can click on your various tabs to reach your information. <br/><br/>
+												<strong>MyStudies</strong>  - See your enrolled Programs and Courses and access the Blackboards for each. <br/>
+												<strong>MyClass timetable</strong>  - View your current timetables for your studies (available once timetabling has been finalised). For more info on timetabling, please check <a href="http://www1.rmit.edu.au/students/timetables">this page.</a> <br/>
+												<strong>MyResults</strong>  - Check the results for your studies once they have been released. <br/>
+												<strong>MyDetails</strong>  - Check you details as registered with RMIT. You can update these by clicking on the "Update my details via Enrolment Online" (EOL) link. If you have any issues with EOL, please contact RMIT Connect for assistance. <br/>
+												<strong>MyServices</strong>  - Access your Transport Concession Form (if eligible) or capture your photo if you do not have one on file already. <br/>
+											</ul>
 										</div>
 									</div>
 								</div>
