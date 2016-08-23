6a447a13a966e8758228c6c224c2b98b4c8fe939
WaiTungYuen <s3372851@student.rmit.edu.au>
2016-08-23 22:41:38 +1000

    - Others fixes

--- includes/results.php
+++ includes/results.php
@@ -30,54 +30,52 @@ if($_GET)
 </div>
 
 <div class="container w">
-    <div class="row centered">
-        <div class="row">
+    <div class="row">
 
-            <?php
+        <?php
 
-            //print "id = ".$id." key = ".$key."<br>";
-            if (strcmp($id, $key) == 0)
+        //print "id = ".$id." key = ".$key."<br>";
+        if (strcmp($id, $key) == 0)
+        {
+            print "<table border='1' id='table'>";
+            print "<thead><tr><th><strong>Id</th><th><strong>First Name</strong></th><th><strong>Last Name</strong></th><th><strong>Email</strong></th><th><strong>OS</strong></th><th><strong>Issue</strong></th><th><strong>Comment</strong></th><th><strong>Status</strong></th><th><strong>Replies/comments</strong></th><th><strong>Update</strong></th></tr></thead>";
+
+            $pdo = TicketPDO::getInstance();
+            $results = $pdo->getData();
+
+            foreach($results as $row)
             {
-                print "<table border='1' id='table'>";
-                print "<thead><tr><th><strong>Id</th><th><strong>First Name</strong></th><th><strong>Last Name</strong></th><th><strong>Email</strong></th><th><strong>OS</strong></th><th><strong>Issue</strong></th><th><strong>Comment</strong></th><th><strong>Status</strong></th><th><strong>Replies/comments</strong></th><th><strong>Update</strong></th></tr></thead>";
+                print "<tr><td>".$row['ticket_id']."</td>";
+                print "<td>".$row['firstName']."</td>";
+                print "<td>".$row['lastName']."</td>";
+                print "<td>".$row['email']."</td>";
+                print "<td>".$row['os']."</td>";
+                print "<td>".$row['issue']."</td>";
+                print "<td>";
 
-                $pdo = TicketPDO::getInstance();
-                $results = $pdo->getData();
-
-                foreach($results as $row)
+                /* split comment string on \n and spit them into their own paragraph */
+                foreach (explode("\n", $row['comments']) as $comment)
                 {
-                    print "<tr><td>".$row['ticket_id']."</td>";
-                    print "<td>".$row['firstName']."</td>";
-                    print "<td>".$row['lastName']."</td>";
-                    print "<td>".$row['email']."</td>";
-                    print "<td>".$row['os']."</td>";
-                    print "<td>".$row['issue']."</td>";
-                    print "<td>";
-
-                    /* split comment string on \n and spit them into their own paragraph */
-                    foreach (explode("\n", $row['comments']) as $comment)
-                    {
-                        print "<p>" . $comment . "</p>";
-                    }
-
-                    print "</td>";
-                    print "<td>".$row['status']."</td>";
-                    print "<td><a href='index.php?page=update_ticket_form.php?id=" . $row['ticket_id'] . "'>Add Comments</a></a></td>";
-                    print "<td><a href='index.php?page=update_ticket_form.php?id=" . $row['ticket_id'] . "'>Update Status</a></a></td></tr>";
+                    print "<p>" . $comment . "</p>";
                 }
 
-                print "</table>";
-
+                print "</td>";
+                print "<td>".$row['status']."</td>";
+                print "<td><a href='index.php?page=update_ticket_form.php?id=" . $row['ticket_id'] . "'>Add Comments</a></a></td>";
+                print "<td><a href='index.php?page=update_ticket_form.php?id=" . $row['ticket_id'] . "'>Update Status</a></a></td></tr>";
             }
 
-            else
+            print "</table>";
 
-            {
-                print "<h1>Could not login.</h1>";
-            }
+        }
 
-            ?>
+        else
 
-        </div><!-- row -->
-    </div>
+        {
+            print "<h1>Could not login.</h1>";
+        }
+
+        ?>
+
+    </div><!-- row -->
 </div><!-- container -->
