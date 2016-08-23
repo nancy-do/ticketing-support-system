a0321d3b9590ad0bdb20ab79b1198b99fe1967e8
blacksfk <s3333113@student.rmit.edu.au>
2016-08-23 17:37:20 +1000

    jquery being hosted on google no need to host locally

--- includes/footer.php
+++ includes/footer.php
@@ -2,12 +2,12 @@
 <!-- FOOTER -->
 <div class="clear"></div>
 <footer id="f">
-	<div class="container">
-		<div class="row centered">
-			<a href="https://twitter.com/?lang=en"><i class="fa fa-twitter"></i></a><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a><a href="fontawesome.io/icon/dribbble/"><i class="fa fa-dribbble"></i></a>
-	
-		</div><!-- row -->
-	</div><!-- container -->
+    <div class="container">
+        <div class="row centered">
+            <a href="https://twitter.com/?lang=en"><i class="fa fa-twitter"></i></a><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a><a href="fontawesome.io/icon/dribbble/"><i class="fa fa-dribbble"></i></a>
+    
+        </div><!-- row -->
+    </div><!-- container -->
 </footer><!-- Footer -->
 
     <!-- Bootstrap core JavaScript
@@ -15,22 +15,21 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
-	<script src="js/toggle.js"></script>
-	<script src="js/jquery.min.js"></script>
-	<script src="js/script.js"></script>
-	<script src="js/validateForm.js"></script>
-	<script src="js/stickyNavbar.js"></script>
-	<script src="js/jsfiddle.js"></script>
-	<script>
-		// Write on keyup event of keyword input element
-		$("#search").keyup(function(){
-			_this = this;
-			// Show only matching TR, hide rest of them
-			$.each($("#table tbody tr"), function() {
-				if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
-					$(this).hide();
-				else
-					$(this).show();
-			});
-		});
-	</script>
+    <script src="js/toggle.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/validateForm.js"></script>
+    <script src="js/stickyNavbar.js"></script>
+    <script src="js/jsfiddle.js"></script>
+    <script>
+        // Write on keyup event of keyword input element
+        $("#search").keyup(function(){
+            _this = this;
+            // Show only matching TR, hide rest of them
+            $.each($("#table tbody tr"), function() {
+                if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+                    $(this).hide();
+                else
+                    $(this).show();
+            });
+        });
+    </script>
