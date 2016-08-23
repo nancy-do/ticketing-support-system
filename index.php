c54a56f0985bb3aa2b0c0dfc64f50f6cbad77635
Nancy <s3510210@student.rmit.edu.au>
2016-08-23 15:41:22 +1000

    Staff Login Implementation

--- index.php
+++ index.php
@@ -11,17 +11,22 @@ if ($page == "faqs")
 {
 	include_once("includes/faqs.php");
 }
-
 else if ($page == "results")
 {
     include_once("includes/results.php");
 }
-
 else if ($page == "staff")
+{
+    include_once("includes/result-view.php");
+}
+else if ($page == "view-ticket")
+{
+    include_once("includes/view-ticket.php");
+}
+else if ($page == "results")
 {
     include_once("includes/results.php");
 }
-
 else
 {
 	include_once("includes/home.php");
