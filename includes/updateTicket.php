be0223809c8a2427a9326ac1ec3d154276e5ded5
Anthony <s3333113@student.rmit.edu.au>
2016-09-02 17:08:17 +1000

    cleaned

--- includes/updateTicket.php
+++ includes/updateTicket.php
@@ -4,8 +4,7 @@ include('TicketPDO.php');
 if ($_POST)
 {
     $pdo = TicketPDO::getInstance();
-    $ticket = new Ticket($_POST["firstName"], $_POST["lastName"], $_POST["email"], $_POST["os"],
-                            $_POST["issue"], $_POST["comments"]);
+    $ticket = new Ticket($_POST["firstName"], $_POST["lastName"], $_POST["email"], $_POST["os"], $_POST["issue"], $_POST["comments"]);
     $pdo->update($ticket);
 
     echo "Success";
