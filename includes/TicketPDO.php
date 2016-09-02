29ef015dbdfeac491cf690602e63dc91e30ea72a
Anthony <s3333113@student.rmit.edu.au>
2016-09-02 20:24:11 +1000

    rewrote pdo to 3 tables

--- includes/TicketPDO.php
+++ includes/TicketPDO.php
@@ -30,22 +30,21 @@ class TicketPDO
      */
     protected function __construct() {
         try {
-            $this->db = new PDO('sqlite:..\tickets.db');      /* WINDOWS */
-            //$this->db = new PDO('sqlite:../tickets.db');        /* LINUX, MAC */
+            //$this->db = new PDO('sqlite:..\tickets.db');      /* WINDOWS */
+            $this->db = new PDO('sqlite:../tickets.db');        /* LINUX, MAC */
 
             // Set errormode to exceptions
             $this->db->setAttribute(PDO::ATTR_ERRMODE,
                 PDO::ERRMODE_EXCEPTION);
             $this->createTables();
         } catch(PDOException $e) {
-            // Print PDOException message
             echo $e->getMessage();
         }
     }
 
     public function createTables() {
         try {
-            $this->db->exec("CREATE TABLE IF NOT EXISTS tickets(
+            /*$this->db->exec("CREATE TABLE IF NOT EXISTS tickets(
                     ticket_id TEXT,
                     firstName TEXT,
                     lastName TEXT,
@@ -53,9 +52,30 @@ class TicketPDO
                     os TEXT,
                     issue TEXT,
                     comments TEXT,
-                    status TEXT)");
+                    status TEXT)");*/
+
+            // TICKET INFO TABLE
+            $this->db->exec("CREATE TABLE IF NOT EXISTS ticketInfo(
+                    ticket_id text PRIMARY KEY,
+                    issue text,
+                    status text)");
+
+            // USER INFO TABLE
+            $this->db->exec("CREATE TABLE IF NOT EXISTS userInfo(
+                    ticket_id text,
+                    firstName text,
+                    lastName text,
+                    email text,
+                    os text,
+                        FOREIGN KEY (ticket_id) REFERENCES ticketInfo(ticket_id)");
+
+            // COMMENTS TABLE (comments are stored as a serialised array)
+            $this->db->exec("CREATE TABLE IF NOT EXISTS comments(
+                    ticket_id text,
+                    comments text,
+                        FOREIGN KEY (ticket_id) REFERENCES ticketInfo(ticket_id)");
+
         } catch(PDOException $e) {
-            // Print PDOException message
             echo $e->getMessage();
         }
     }
@@ -67,7 +87,7 @@ class TicketPDO
         try {
             // Prepare INSERT statement to SQLite3 file db
 
-            $insert = "INSERT INTO tickets (ticket_id, firstName, lastName, email, os, issue, comments, status)
+            /*$insert = "INSERT INTO tickets (ticket_id, firstName, lastName, email, os, issue, comments, status)
                 VALUES (:ticket_id, :firstName, :lastName, :email, :os, :issue, :comments, :status)";
 
             $ticket_id = $ticket->getId();
@@ -94,7 +114,35 @@ class TicketPDO
             $sql->bindParam(':issue', $issue);
             $sql->bindParam(':comments', $commentString);
             $sql->bindParam(':status', $status);
+            $sql->execute();*/
+
+            // TICKET INFO INSERT
+            $insert = "INSERT INTO ticketInfo(ticket_id, issue, status)
+                        VALUES (:ticket_id, :issue, :status)";
+            $sql = $this->db->prepare($insert);
+            $sql->bindParam(":ticket_id", $ticket->getID());
+            $sql->bindParam(":issue", $ticket->getIssue());
+            $sql->bindParam(":status", $ticket->getStats());
             $sql->execute();
+
+            // USER INFO INSERT
+            $insert = "INSERT INTO userInfo(ticket_id, firstName, lastName, email, os)
+                        VALUES (:ticket_id, :firstName, :lastName, :email, :os)";
+            $sql = $this->db->prepare($insert);
+            $sql->bindParam(":ticket_id", $ticket->getID());
+            $sql->bindParam(":firstName", $ticket->getFirstName());
+            $sql->bindParam(":lastName", $ticket->getLastName());
+            $sql->bindParam(":email", $ticket->getEmail());
+            $sql->bindParam(":os", $ticket->getOS());
+            $sql->execute();
+
+            // COMMENTS INSERT
+            $insert = "INSERT INTO comments(ticket_id, comments) VALUES (:ticket_id, :comments)";
+            $sql = $this->db->prepare($insert);
+            $sql->bindParam(":ticket_id", $ticket->getID());
+            $sql->bindParam(":comments", serialize($ticket->getComments()));
+            $sql->execute();
+
         } catch(PDOException $e) {
             // Print PDOException message
             echo $e->getMessage();
@@ -103,23 +151,39 @@ class TicketPDO
 
     public function getData()
     {
-        try {
+        try
+        {
+            $result = $this->db->query("SELECT * FROM ticketInfo");
+            echo "ticketInfo: " . $result . "\n";
+
+            $result = $this->db->query("SELECT * FROM userInfo");
+            echo "userInfo: " . $result . "\n";
+
+            $result = $this->db->query("SELECT * FROM comments");
+            echo "comments: " . $result . "\n";
+        }
+        catch (PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+        /*try {
             // Prepare INSERT statement to SQLite3 file db
             $result = $this->db->query('SELECT * FROM tickets');
             return $result;
         } catch (PDOException $e) {
             // Print PDOException message
             echo $e->getMessage();
-        }
+        }*/
     }
 
     public function getIdData($id)
     {
         try {
-            // Prepare INSERT statement to SQLite3 file db
+            /*// Prepare INSERT statement to SQLite3 file db
             $result = $this->db->query('SELECT firstName,lastName,email, os, issue, comments FROM tickets
                         WHERE ticket_id="'.$id.'"');
-            return $result;
+            return $result;*/
+
         } catch (PDOException $e) {
             // Print PDOException message
             echo $e->getMessage();
