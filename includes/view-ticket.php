396ff9f51cb7f9f9f2ebeaeabd8e91c62896a362
Anthony <s3333113@student.rmit.edu.au>
2016-09-03 16:32:59 +1000

    added more error handling

--- includes/view-ticket.php
+++ includes/view-ticket.php
@@ -8,40 +8,43 @@
 
 include('TicketPDO.php');
 
-if($_GET)
+$error = "<h3>ID DOES NOT EXIST</h3><p><strong>WARNING - You have entered an incorrect Ticket ID</strong></p>";
+
+if (!isset($_GET['ticketID']))
 {
-    $id = $_GET['ticketID'];
-
-    $pdo = TicketPDO::getInstance();
-    $ticket = $pdo->getIdData($id);
-
-    if (!isset($ticket))
-    {
-        echo "<h3>ID DOES NOT EXIST</h3><p><strong>WARNING - You have entered an incorrect Ticket ID</strong></p>";
-        return;
-    }
-
-    $string = "<div class='container w'><div class='row centered'><div class='col-lg-12'>";
-    $string .= "<table border=1>";
-    $string .= "<thead><tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>OS</th><th>Issue</th><th>Comment</th><th>Status</th></tr></thead>";
-
-    $string .= "<tbody><tr>";
-    $string .= "<td>" . $id . "</td>";
-    $string .= "<td>" . $ticket->getFirstName() . "</td>";
-    $string .= "<td>" . $ticket->getLastName() . "</td>";
-    $string .= "<td>" . $ticket->getEmail() . "</td>";
-    $string .= "<td>" . $ticket->getOS() . "</td>";
-    $string .= "<td>" . $ticket->getIssue() . "</td>";
-    $string .= "<td>";
-
-    foreach ($ticket->getComments() as $comment)
-    {
-        $string .= $comment . "\n";
-    }
-
-    $string .= "</td>";
-    $string .= "<td>" . $ticket->getStatus() . "</td>";
-    $string .= "</tr></tbody></table>";
-
-    echo $string;
+    echo $error;
 }
+
+$id = $_GET['ticketID'];
+
+$pdo = TicketPDO::getInstance();
+$ticket = $pdo->getIdData($id);
+
+if (!isset($ticket))
+{
+    echo $error;
+}
+
+$string = "<div class='container w'><div class='row centered'><div class='col-lg-12'>";
+$string .= "<table border=1>";
+$string .= "<thead><tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>OS</th><th>Issue</th><th>Comment</th><th>Status</th></tr></thead>";
+
+$string .= "<tbody><tr>";
+$string .= "<td>" . $id . "</td>";
+$string .= "<td>" . $ticket->getFirstName() . "</td>";
+$string .= "<td>" . $ticket->getLastName() . "</td>";
+$string .= "<td>" . $ticket->getEmail() . "</td>";
+$string .= "<td>" . $ticket->getOS() . "</td>";
+$string .= "<td>" . $ticket->getIssue() . "</td>";
+$string .= "<td>";
+
+foreach ($ticket->getComments() as $comment)
+{
+    $string .= $comment . "\n";
+}
+
+$string .= "</td>";
+$string .= "<td>" . $ticket->getStatus() . "</td>";
+$string .= "</tr></tbody></table>";
+
+echo $string;
