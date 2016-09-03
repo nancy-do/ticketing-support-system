24d9ffb8b98d7fd74528d4771ba401915db793c0
Anthony <s3333113@student.rmit.edu.au>
2016-09-04 02:00:49 +1000

    now dynamically calls db updates based on what changed

--- includes/updateTicket.php
+++ includes/updateTicket.php
@@ -7,28 +7,27 @@ if (!isset($_POST))
     return;
 }
 
-$return = "";
+$pdo = TicketPDO::getInstance();
+$commentsArray = explode("\n", $_POST["comments"]);
 
-foreach ($_POST as $val)
+$updTicket = new Ticket("lol", $_POST["lastname"], $_POST["email"], $_POST["os"], $_POST["issue"], $_POST["status"], $commentsArray, $_POST["id"]);
+$oldTicket = $pdo->getIdData($_POST["id"]);
+$updArr = $updTicket->getVars();
+$oldArr = $oldTicket->getVars();
+$whatsNew = array_diff_assoc($updArr, $oldArr);
+
+// Check whats changed and this way limit the database calls for security etc ID SHOULD NEVER CHANGE ON TICKET UPDATE
+if (array_key_exists("status", $whatsNew) || array_key_exists("issue", $whatsNew))
 {
-    $return .= $val . "\n";
+    $pdo->updateTicket($updTicket);
 }
 
-echo $return;
-
-// $pdo = TicketPDO::getInstance();
-// $ticket = new Ticket()
-
-
-/*if ($_POST)
+if (array_key_exists("firstName", $whatsNew) || array_key_exists("lastName", $whatsNew) || array_key_exists("email", $whatsNew) || array_key_exists("os", $whatsNew))
 {
-    $pdo = TicketPDO::getInstance();
-    $ticket = new Ticket($_POST["firstName"], $_POST["lastName"], $_POST["email"], $_POST["os"], $_POST["issue"], $_POST["comments"]);
-    $pdo->update($ticket);
-
-    echo "Success";
+    $pdo->updateUser($updTicket);
 }
-else
+
+if (array_key_exists("comments", $whatsNew))
 {
-    echo "Invalid data posted";
-}*/
+    $pdo->updateComments($updTicket);
+}
