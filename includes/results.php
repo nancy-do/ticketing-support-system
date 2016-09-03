aabc61ecd0af44071dba8d8526a80021fe2fe800
Anthony <s3333113@student.rmit.edu.au>
2016-09-03 14:31:40 +1000

    concatenates a string and echos instead of a million echos

--- includes/results.php
+++ includes/results.php
@@ -21,10 +21,9 @@ if($_GET)
 
         <?php
 
-        //print "id = ".$id." key = ".$key."<br>";
         if (strcmp($id, $key) == 0)
         {
-            echo '<div class="col-sm-6 col-sm-offset-3">
+            $string = '<div class="col-sm-6 col-sm-offset-3">
                     <div class="input-group stylish-input-group">
                         <input type="text" id="search" class="form-control"  placeholder="Search" >
                             <span class="input-group-addon">
@@ -36,7 +35,7 @@ if($_GET)
                   </div>
 
             <table border="1" id="table">
-            <thead><tr><th><strong>Id</th><th><strong>First Name</strong></th><th><strong>Last Name</strong></th><th><strong>Email</strong></th><th><strong>OS</strong></th><th><strong>Issue</strong></th><th><strong>Comment</strong></th><th><strong>Status</strong></th><th><strong>Replies/comments</strong></th><th><strong>Update</strong></th></tr></thead></table>';
+            <thead><tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>OS</th><th>Issue</th><th>Comment</th><th>Status</th><th>Replies/comments</th><th>Update</th></tr></thead></table>';
 
             $pdo = TicketPDO::getInstance();
             $ticketIDs = $pdo->getIDs();
@@ -45,59 +44,30 @@ if($_GET)
             {
                 $ticket = $pdo->getIdData($id['ticket_id']);
 
-                echo "<table><tr>";
-                echo "<td>" . $id['ticket_id'] . "</td>";
-                echo "<td>" . $ticket->getFirstName() . "</td>";
-                echo "<td>" . $ticket->getLastName() . "</td>";
-                echo "<td>" . $ticket->getEmail() . "</td>";
-                echo "<td>" . $ticket->getOS() . "</td>";
-                echo "<td>" . $ticket->getIssue() . "</td>";
-                echo "<td>";
+                $string .= "<table><tr>";
+                $string .= "<td>" . $id['ticket_id'] . "</td>";
+                $string .= "<td>" . $ticket->getFirstName() . "</td>";
+                $string .= "<td>" . $ticket->getLastName() . "</td>";
+                $string .= "<td>" . $ticket->getEmail() . "</td>";
+                $string .= "<td>" . $ticket->getOS() . "</td>";
+                $string .= "<td>" . $ticket->getIssue() . "</td>";
+                $string .= "<td>";
 
                 foreach ($ticket->getComments() as $comment)
                 {
-                    echo $comment . "\n";
+                    $string .= $comment . "\n";
                 }
 
-                echo "</td>";
-                echo "<td>" . $ticket->getStatus() . "</td>";
-                echo "</tr></table>";
+                $string .= "</td>";
+                $string .= "<td>" . $ticket->getStatus() . "</td>";
+                $string .= "</tr></table>";
             }
 
-            /*foreach($results as $row)
-            {
-                print "<tr><td>".$row['ticket_id']."</td>";
-                print "<td>".$row['firstName']."</td>";
-                print "<td>".$row['lastName']."</td>";
-                print "<td>".$row['email']."</td>";
-                print "<td>".$row['os']."</td>";
-                print "<td>".$row['issue']."</td>";
-                print "<td>";
-
-                // split comment string on \n and spit them into their own paragraph
-                foreach (explode("\n", $row['comments']) as $comment)
-                {
-                    print "<p>" . $comment . "</p>";
-                }
-
-                print "</td>";
-                print "<td>".$row['status']."</td>";
-                print "<td><a href='index.php?page=update_ticket_form.php?id=" . $row['ticket_id'] . "'>Add Comments</a></a></td>";
-                print "<td><a href='index.php?page=update_ticket_form.php?id=" . $row['ticket_id'] . "'>Update Status</a></a></td></tr>";
-                print "</tbody>";
-            }
-
-            print "</table>";*/
-
+            echo $string;
         }
-
         else
-
         {
-            print "<div class=\"container w\"><div class=\"row centered\"><div class=\"col-lg-12\">";
-            print "<h3>COULD NOT LOGIN</h3>";
-            print "<strong><p>WARNING - You have entered an incorrect password</p></strong>";
-            print "</div></div></div>";
+            echo "<div class='container w'><div class='row centered'><div class='col-lg-12'><h3>COULD NOT LOGIN</h3><p><strong>WARNING - You have entered an incorrect password</strong></p></div></div></div>";
         }
 
         ?>
