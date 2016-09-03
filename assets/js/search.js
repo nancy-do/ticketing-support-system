549d7fefa5d44bc667b9ae235730b721291a37d4
Anthony <s3333113@student.rmit.edu.au>
2016-09-03 17:35:02 +1000

    removed debug lines

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,7 +3,7 @@ $(document).on( 'keyup', '#search', function()
     _this = this;
     // Show only matching TR, hide rest of them
     $.each($("#table tbody").find("tr"), function() {
-        console.log($(this).text());
+        //console.log($(this).text());
         if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) == -1)
             $(this).hide();
         else
