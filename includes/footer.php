dfeb2b1932739d037fad9775f9e3344c80a01bee
Anthony <s3333113@student.rmit.edu.au>
2016-09-03 16:29:14 +1000

    removed old script

--- includes/footer.php
+++ includes/footer.php
@@ -19,22 +19,5 @@
     <script src="assets/js/validateForm.js"></script>
     <script src="assets/js/stickyNavbar.js"></script>
     <script src="assets/js/jsfiddle.js"></script>
-    <script>
-        // Write on keyup event of keyword input element
-        $( document ).ready(function(){
-            // Write on keyup event of keyword input element
-            $("#search").keyup(function(){
-                _this = this;
-                // Show only matching TR, hide rest of them
-                $.each($("#table tbody").find("tr"), function() {
-                    console.log($(this).text());
-                    if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) == -1)
-                        $(this).hide();
-                    else
-                        $(this).show();
-                });
-            });
-        });
-    </script>
 </body>
 </html>
