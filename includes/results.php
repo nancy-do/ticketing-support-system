ff27248519f78ee70a766df420b72242d38cb510
Nancy <s3510210@student.rmit.edu.au>
2016-09-04 06:25:04 +1000

    Change ticket display to 1 table

--- includes/results.php
+++ includes/results.php
@@ -18,22 +18,31 @@ if($_GET)
 ?>
 
 <div class="container w">
-    <div class="row">
-
         <?php
 
         if (strcmp($id, $key) == 0)
         {
-            $string = '<div class="col-sm-6 col-sm-offset-3">
-                    <div class="input-group stylish-input-group">
-                        <input type="text" id="search" class="form-control"  placeholder="Search" >
-                            <span class="input-group-addon">
-                                <button type="submit">
-                                    <span class="glyphicon glyphicon-search"></span>
-                                </button>
-                            </span>
-                    </div>
-                  </div>';
+            $string = ' <div class="row"><div class="col-sm-6 col-sm-offset-3">
+                            <div class="input-group stylish-input-group">
+                                <input type="text" id="search" class="form-control"  placeholder="Search" >
+                                <span class="input-group-addon">
+                                    <button type="submit">
+                                        <span class="glyphicon glyphicon-search"></span>
+                                    </button>
+                                </span>
+                            </div>
+                        </div><!-- row --></div>
+                        <table border="1" id="table">
+                            <thead><tr><th>Id</th>
+                                       <th>First Name</th>
+                                       <th>Last Name</th>
+                                       <th>Email</th>
+                                       <th>OS</th>
+                                       <th>Issue</th>
+                                       <th>Comments</th>
+                                       <th>Status</th>
+                                       <th>Ticket Management</th>
+                            </tr></thead>';
 
             /*<table border="1" id="table">
             <thead><tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>OS</th><th>Issue</th><th>Comments</th><th>Status</th><th>Ticket Management</th></tr></thead><tbody>';*/
@@ -44,8 +53,6 @@ if($_GET)
             foreach ($ticketIDs as $id)
             {
                 $ticket = $pdo->getIdData($id['ticket_id']);
-
-                $string .= "<table><thead><tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>OS</th><th>Issue</th><th>Comments</th><th>Status</th><th>Ticket Management</th></tr></thead>";
                 $string .= "<tr><tbody>";
                 $string .= "<td>" . $id['ticket_id'] . "</td>";
                 $string .= "<td>" . $ticket->getFirstName() . "</td>";
@@ -62,9 +69,9 @@ if($_GET)
 
                 $string .= "</td>";
                 $string .= "<td>" . $ticket->getStatus() . "</td>";
-                $string .= "<td><button class='btn btn-danger editTicket'>Manage</button></td></tr></tbody></table>";
+                $string .= "<td><button class='btn btn-danger editTicket'>Manage</button></td></tbody></tr>";
             }
-
+            $string .= "</table>";
             //$string .= "</tbody></table>";
 
             // for staff ticket editing
@@ -94,5 +101,5 @@ if($_GET)
         }
 
         ?>
-    </div><!-- row -->
+
 </div><!-- container -->
