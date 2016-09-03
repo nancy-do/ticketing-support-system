5da2601abc9ab542fc5ed960ee69ee3ec6637f03
Anthony <s3333113@student.rmit.edu.au>
2016-09-03 17:02:28 +1000

    no need for document ready wrapper

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -4,49 +4,46 @@
 
 const FADE_TIME = 500;
 
-$(document).ready(function()
+$(document).on('submit', '#reg-form', function()
 {
-    $(document).on('submit', '#reg-form', function()
-    {
-        var data = $(this).serialize();
+    var data = $(this).serialize();
 
-        $.get("includes/submit.php", data, function(returnData) {
-            $("#reg-ticket").fadeOut(FADE_TIME);
-            $("#reg-form").fadeOut(FADE_TIME);
-            $(":animated").promise().done(function() {
-                $(returnData).hide().appendTo(".result").fadeIn(FADE_TIME);
-            })
+    $.get("includes/submit.php", data, function(returnData) {
+        $("#reg-ticket").fadeOut(FADE_TIME);
+        $("#reg-form").fadeOut(FADE_TIME);
+        $(":animated").promise().done(function() {
+            $(returnData).hide().appendTo(".result").fadeIn(FADE_TIME);
         })
+    })
 
-        return false;
-    });
+    return false;
+});
 
-    $(document).on('submit', '#viewform', function()
-    {
-        var data = $(this).serialize();
+$(document).on('submit', '#viewform', function()
+{
+    var data = $(this).serialize();
 
-        $.get("includes/view-ticket.php", data, function(returnData) {
-            $("#submit-info").fadeOut(FADE_TIME);
-            $("#view-info").fadeOut(FADE_TIME);
-            $(":animated").promise().done(function() {
-                $(returnData).hide().appendTo("#view-results").fadeIn(FADE_TIME);
-            })
+    $.get("includes/view-ticket.php", data, function(returnData) {
+        $("#submit-info").fadeOut(FADE_TIME);
+        $("#view-info").fadeOut(FADE_TIME);
+        $(":animated").promise().done(function() {
+            $(returnData).hide().appendTo("#view-results").fadeIn(FADE_TIME);
         })
+    })
 
-        return false;
-    });
+    return false;
+});
 
-    $(document).on('submit', '#staff-login-form', function()
-    {
-        var data = $(this).serialize();
+$(document).on('submit', '#staff-login-form', function()
+{
+    var data = $(this).serialize();
 
-        $.get("includes/results.php", data, function(returnData) {
-            $("#staff-login-container").fadeOut(FADE_TIME);
-            $(":animated").promise().done(function() {
-                $(returnData).hide().appendTo("#staff-results-container").fadeIn(FADE_TIME);
-            })
+    $.get("includes/results.php", data, function(returnData) {
+        $("#staff-login-container").fadeOut(FADE_TIME);
+        $(":animated").promise().done(function() {
+            $(returnData).hide().appendTo("#staff-results-container").fadeIn(FADE_TIME);
         })
+    })
 
-        return false;
-    });
+    return false;
 });
