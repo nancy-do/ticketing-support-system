21f4fb89d074ad106d73114f8492f455c0481403
WaiTung Yuen <s3372851@student.rmit.edu.au>
2016-09-04 14:48:44 +1000

    - Others fixes

--- includes/footer.php
+++ includes/footer.php
@@ -14,6 +14,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/toggle.js"></script>
     <script src="assets/js/submit.js"></script>
     <script src="assets/js/search.js"></script>
